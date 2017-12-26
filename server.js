74852338fae16f9c24442fe0bd316e3d42295fe5
JuneAlSi <31959123+JuneAlSi@users.noreply.github.com>
2017-12-26 22:41:13 +0700

    Prep repo for Heroku

--- server.js
+++ server.js
@@ -2,11 +2,24 @@ var express = require('express');
 
 // Create our app
 var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(function(req, res, next){
+  if(req.headers['x-forwarded-proto'] === 'http'){
+    next();
+  } else{
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
 
 // tell which folder want tobe served
 app.use(express.static('public'));
 
 // start server
-app.listen(3000, function(){
-  console.log('Express is up on port 3000');
+// app.listen(3000, function(){
+//   console.log('Express is up on port 3000');
+// });
+
+app.listen(PORT, function(){
+  console.log('Express server is up on port ' + PORT);
 });
